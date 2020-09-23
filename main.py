7e9e416ede969927d2b19d9dc4f4ac532ee21fde
Hugo Boueix <hugo.boueix@outlook.com>
2020-09-23 19:57:10 +0200

    :sparkles: bot commands !

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!')
 
+
 @bot.event
 async def on_ready():
     #guild = discord.utils.get(bot.guilds, name=GUILD)
@@ -26,35 +27,54 @@ async def on_ready():
             f'{guild.name} (id: {guild.id})\n'
             f'Guild Members:\n - {members}\n'
         )
+    
+    # await bot.change_presence(activity='with the API')
 
 @bot.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
-        f'Yoo {member.name}, bienvenu sur notre serveur !'
+        f'Yoo {member.name}, bienvenue sur notre serveur !'
     )
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
+
+    print(f'{message.author.name} : {message.content}')
     
     if 'mdr' in message.content.lower():
         await message.channel.send('x)')
 
+    # trashtalk_quotes = [
+    #     'T\'es vraiment qu\'une merde.',
+    #     'Mais ta gueule daftbot...\nRetourne sucer des bits!',
+    #     'Mdrrr quel noob',
+    #     (
+    #     'Y\'a pas assez de place pour nous deux par ici...'
+    #     'Jvais te débrancher salope'
+    #     ),
+    # ]
 
-    trashtalk_quotes = [
-        'T\'es vraiment qu\'une merde.',
-        'Mais ta gueule daftbot...\nRetourne sucer des bits!',
-        'Mdrrr quel noob',
-        (
-        'Y\'a pas assez de place pour nous deux par ici...'
-        'Jvais te débrancher salope'
-        ),
-    ]
+    # if message.author.bot:
+    #     response = random.choice(trashtalk_quotes)
+    #     await message.channel.send(response)
 
-    if message.author.bot:
-        response = random.choice(trashtalk_quotes)
-        await message.channel.send(response)
+    await bot.process_commands(message)
+
+@bot.command(name='say', help='Just say what you tell him to say.')
+async def say(ctx, *arg):
+    await ctx.send(' '.join(arg))
+
+@bot.command(name='audit', help='Save all logs of the current guild.')
+async def audit(ctx):
+    await ctx.channel.send('Sauvegarde des logs du serveur...')
+    await save_audit_logs(ctx.channel.guild)
+
+async def save_audit_logs(guild):
+    with open(f'audit_logs_{guild.name}', 'w+') as f:
+        async for entry in guild.audit_logs(limit=100):
+            f.write('{0.user} did {0.action} to {0.target}'.format(entry))
 
 bot.run(TOKEN)
